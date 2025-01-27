90332963dca0e64499b84d0f79e9c2d42b34b548
Gleb <w>
2025-01-27 19:33:25 +0300

    Initial Commit

---
+++ App_0201/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  App_0201
+//
+//  Created by Глеб Соколов on 27.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
