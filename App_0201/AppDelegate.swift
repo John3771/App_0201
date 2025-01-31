71544a7a891fd0ccfbe403826327b280b5f3e52c
Gleb <w>
2025-01-31 22:18:28 +0300

    Setup LaunchScreen

--- App_0201/AppDelegate.swift
+++ App_0201/AppDelegate.swift
@@ -26,6 +26,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         
+        class GeldLubitHui {
+        
+        }
     }
 
 
